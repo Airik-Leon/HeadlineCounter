ef82ed7ac926a4a472f694c904291dce73c06c13
Airik Leon <airik.leon@gmail.com>
2016-04-15 07:40:43 -0500

    Cleaned up code arrangement *Update*

--- Main.cpp
+++ Main.cpp
@@ -2,76 +2,54 @@
 #include <string>
 using namespace std; 
 
-int main() {
-double sum = 0;
-string headline;
-char again;  
-do{
-  cout << "Enter your Headline all on one line as it appears (even if in decks): "<<endl;
+int main()
+{
+  double sum = 0;
+  string headline;
+  char again;  
+  do{
+    cout << "Enter your Headline all on one line as it appears (even if in decks): "<<endl;
     getline(cin, headline);
-  for (int i = 0; i < headline.length(); i++) {
-    if (headline[i] == 'f'
-      || headline[i] == 'i'
-      || headline[i] == 'j'
-      || headline[i] == 'l'
-      || headline[i] == 't'
-      || headline[i] == 'I'
-      || headline[i] == '1'
-      || headline[i] == ':'
-      || headline[i] == ';'
-      || headline[i] == '\''
-      || headline[i] == '('
-      || headline[i] == ')'
-      || headline[i] == '!'
-      || headline[i] == '.'
-      || headline[i] == ',')
+    for (int i = 0; i < headline.length(); i++) 
+    {
+      if (headline[i] == 'f'|| headline[i] == 'i'|| headline[i] == 'j'
+      || headline[i] == 'l'|| headline[i] == 't'|| headline[i] == 'I'
+      || headline[i] == '1'|| headline[i] == ':'|| headline[i] == ';'
+      || headline[i] == '\''|| headline[i] == '('|| headline[i] == ')'
+      || headline[i] == '!'|| headline[i] == '.'|| headline[i] == ',')
     {
       sum = sum + .5;
     }
-     else if(headline[i] == 'w'
-       || headline[i] == 'm'
-       || headline[i] == 'A'
-       || headline[i] == 'B'
-       || headline[i] == 'C'
-       || headline[i] == 'D'
-       || headline[i] == 'E'
-       || headline[i] == 'F'
-       || headline[i] == 'G'
-       || headline[i] == 'H'
-       || headline[i] == 'J'
-       || headline[i] == 'K'
-       || headline[i] == 'L'
-       || headline[i] == 'N'
-       || headline[i] == 'O'
-       || headline[i] == 'P'
-       || headline[i] == 'Q'
-       || headline[i] == 'R'
-       || headline[i] == 'S'
-       || headline[i] == 'T'
-       || headline[i] == 'U'
-       || headline[i] == 'V'
-       || headline[i] == 'X'
-       || headline[i] == 'Y'
-       || headline[i] == 'Z'
-       || headline[i] == '-'
-       || headline[i] == '$'
-       || headline[i] == '%'
-       || headline[i] == '&')
-             {
-               sum = sum + 1.5; 
-             }
-    else if( headline[i] == (int) ' ')
-    sum = sum + 1; 
+     else if(headline[i] == 'w'|| headline[i] == 'm'|| headline[i] == 'A'
+       || headline[i] == 'B'|| headline[i] == 'C'|| headline[i] == 'D'
+       || headline[i] == 'E'|| headline[i] == 'F'|| headline[i] == 'G'
+       || headline[i] == 'H'|| headline[i] == 'J'|| headline[i] == 'K'
+       || headline[i] == 'L'|| headline[i] == 'N'|| headline[i] == 'O'
+       || headline[i] == 'P'|| headline[i] == 'Q'|| headline[i] == 'R'
+       || headline[i] == 'S'|| headline[i] == 'T'|| headline[i] == 'U'
+       || headline[i] == 'V' || headline[i] == 'X' || headline[i] == 'Y'
+       || headline[i] == 'Z' || headline[i] == '-' || headline[i] == '$'
+       || headline[i] == '%' || headline[i] == '&')
+      {
+        sum = sum + 1.5; 
+      }
+     else if( headline[i] == (int) ' ')
+     {
+       sum = sum + 1; 
+     }
      else
-         sum = sum + 1; 
+       sum = sum + 1; 
     } 
   cout<<"The count for this Headline is: "<<sum<<endl; 
   cout<<"Another headline? Enter Y/N "; 
   cin>>again; 
-  if(again == 'y' || again == 'Y')
+  if
+  {
   cout<<"Okay, keep working hard. "<<endl; 
+  }
   else 
   exit(0); 
+  
   cin.ignore();
   sum = 0; 
  } while(1);
